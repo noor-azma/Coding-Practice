87dec7e04b8fabbc9522910d743dadedaac380e4
noor-azma <89718030+noor-azma@users.noreply.github.com>
2022-08-23 19:48:59 +0530

    Create Longest Common Substring.java

---
+++ Dynamic Programming/Medium level/Longest Common Substring.java
@@ -0,0 +1,68 @@
+QUESTION LINK - https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1
+VIDEO LINK - https://www.youtube.com/watch?v=HrybPYpOvz0&list=PL_z_8CaSLPWekqhdCPmFohncHwz8TY2Go&index=22
+
+========================================================ANSWER=====================================================================================
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String input[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(input[0]);
+            int m = Integer.parseInt(input[1]);
+            
+            String S1 = read.readLine().trim();
+            String S2 = read.readLine().trim();
+
+            Solution ob = new Solution();
+            System.out.println(ob.longestCommonSubstr(S1, S2, n, m));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    int longestCommonSubstr(String S1, String S2, int n, int m){
+        
+        int dp[][] = new int[n+1][m+1];
+        
+        for(int i=0; i<dp.length; i++){
+            
+            for(int j=0; j<dp[0].length; j++){
+                
+             if(i==0 || j==0) dp[i][j]=0;
+             
+            }
+        }
+        
+        int max = Integer.MIN_VALUE;
+        
+         for(int i=1; i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+            
+                if(S1.charAt(i-1) == S2.charAt(j-1)){
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                }
+                
+                else dp[i][j] =0;
+                
+                max = Math.max(max,dp[i][j]);
+            
+            }
+        }
+        
+        return max;
+    }
+}
