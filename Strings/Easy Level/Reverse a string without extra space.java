8357c90829c5ea6de3c62e0ca5d5ea56c1daa5b5
noor-azma <89718030+noor-azma@users.noreply.github.com>
2022-09-01 00:35:05 +0530

    Create Reverse a string without extra space.java

---
+++ Strings/Easy Level/Reverse a string without extra space.java
@@ -0,0 +1,21 @@
+QUESTION LINK - https://leetcode.com/problems/reverse-string/
+APPROACH - Swapping the extremes
+
+=============================================================================ANSWER====================================================================================
+
+class Solution {
+    public void reverseString(char[] s) {
+        
+        int i=0;
+        int j = s.length-1;
+        
+        while(i<=j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            
+            i++;
+            j--;
+        }
+    }
+}
