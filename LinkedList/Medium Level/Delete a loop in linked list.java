448a215c9f3965002311c88eeead2781c1047d42
noor-azma <89718030+noor-azma@users.noreply.github.com>
2022-09-04 02:56:53 +0530

    Create Delete a loop in linked list.java

---
+++ LinkedList/Medium Level/Delete a loop in linked list.java
@@ -0,0 +1,45 @@
+QUESTION LINK - https://practice.geeksforgeeks.org/problems/remove-loop-in-linked-list/1
+VIDEO LINK - https://www.youtube.com/watch?v=aIR0s1tY2Vk
+
+========================================================ANSWER=====================================================================================
+
+class Solution
+{
+    //Function to remove a loop in the linked list.
+    public static void removeLoop(Node head){
+        
+        Node slow = head;
+        Node fast = head;
+        
+        while( fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            
+            if(slow == fast) {
+                break;
+            }
+        }
+        
+        if(slow == head && fast == head){
+            
+            while(fast.next != slow){
+                fast = fast.next;
+            }
+            
+            fast.next = null;
+        }
+        
+        else if(slow == fast){
+            slow = head;
+            while(slow.next != fast.next){
+                slow = slow.next;
+                fast = fast.next;
+            }
+            fast.next = null;
+        }
+        
+    
+    }
+}
+
+============================================CAN ALSO BE DONE USING HASHMAP(to implement it watch the video and also detect a loop video of same guy===================
