62b443bdbd7a9e4584972d69b11ac3df3cf7fc6d
noor-azma <89718030+noor-azma@users.noreply.github.com>
2022-09-15 12:01:12 +0530

    Create Level order traversal.java

---
+++ Trees/Easy Level/Level order traversal.java
@@ -0,0 +1,33 @@
+QUESTION LINK - https://practice.geeksforgeeks.org/problems/level-order-traversal/1
+VIDEO LINK - https://www.youtube.com/watch?v=ICpp88mcB-k
+
+========================================================ANSWER=====================================================================================
+  
+class Solution
+{
+    //Function to return the level order traversal of a tree.
+    static ArrayList <Integer> levelOrder(Node node) 
+    {
+        ArrayList<Integer> ans = new ArrayList<>();
+        
+        Queue<Node> q = new LinkedList<>();
+        
+        q.add(node);
+        
+        while(!q.isEmpty()){
+            
+            Node current = q.poll();
+            ans.add(current.data);
+            
+            if(current.left!= null){
+                q.add(current.left);
+            }
+            
+             if(current.right!= null){
+                q.add(current.right);
+            }
+        }
+        
+        return ans;
+    }
+}
