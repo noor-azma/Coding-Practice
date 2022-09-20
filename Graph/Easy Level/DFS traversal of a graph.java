8171c1381148b6a0274d7600070e6fb734232c04
noor-azma <89718030+noor-azma@users.noreply.github.com>
2022-09-20 16:32:51 +0530

    Update DFS traversal of a graph.java

--- Graph/Easy Level/DFS traversal of a graph.java
+++ Graph/Easy Level/DFS traversal of a graph.java
@@ -1,4 +1,5 @@
 QUESTION LINK - https://practice.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+VIDEO LINK - STRIVER (FOR UNCONNECTED GRAPH)
 
 ========================================================ANSWER=====================================================================================
   
@@ -25,5 +26,40 @@ class Solution {
             }
         }
         return ans;
+    }
+}
+
+================================FOR UNCONNECED GRAPH=============
+
+
+class Solution {
+    // Function to return a list containing the DFS traversal of the graph.
+    public ArrayList<Integer> dfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        boolean visited[] = new boolean[V];
+        
+        for(int i=0; i<V; i++){
+            
+            if(visited[i] == false){
+                helper(i, adj, visited, ans);
+            }
+        }
+        
+        return ans;
+    }
+    
+    public void helper(int j, ArrayList<ArrayList<Integer>> adj, boolean visited[], ArrayList<Integer> ans){
+        
+        ans.add(j);
+        visited[j] = true;
+        
+        for(Integer i : adj.get(j)){
+            
+            if(visited[i] == false){
+                helper(i, adj, visited, ans);
+            }
+        }
+        
     }
 }
