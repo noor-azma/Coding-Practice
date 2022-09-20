fa46bfc78f6b35f016165e2f7829546d88de5b0c
noor-azma <89718030+noor-azma@users.noreply.github.com>
2022-09-20 20:03:09 +0530

    Update Detect Cycle in Undirected.java

--- Graph/Medium Level/Detect Cycle in Undirected.java
+++ Graph/Medium Level/Detect Cycle in Undirected.java
@@ -51,3 +51,37 @@ class Solution {
     
     
 }
+
+=====================================USING DFS==========================================
+  class Solution {
+   // Function to detect cycle in an undirected graph.
+   public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+       // Code here
+       
+       boolean visited[]=new boolean[V];
+       for(int i=0;i<V;i++)
+       {
+           if(!visited[i])
+           if(isCyclic(visited,i,adj,-1))
+           return true;
+           
+       }
+       return false;
+   }
+   public boolean isCyclic(boolean visited[],int child,ArrayList<ArrayList<Integer>> adj,int parent)
+   {
+       visited[child]=true;
+       
+       for(int i=0;i<adj.get(child).size();i++)
+       {
+           if(!visited[adj.get(child).get(i)])
+           {
+           if(isCyclic(visited,adj.get(child).get(i),adj,child))
+           return true;
+           }
+           else if(adj.get(child).get(i)!=parent)
+           return true;
+       }
+       return false;
+   }
+}
