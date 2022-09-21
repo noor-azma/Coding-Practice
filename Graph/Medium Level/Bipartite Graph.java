5bf93553bee592ef6948dd68b73cd4bdb52df9b7
noor-azma <89718030+noor-azma@users.noreply.github.com>
2022-09-21 17:58:25 +0530

    Update Bipartite Graph.java

--- Graph/Medium Level/Bipartite Graph.java
+++ Graph/Medium Level/Bipartite Graph.java
@@ -1,5 +1,6 @@
 QUESTION LINK - https://practice.geeksforgeeks.org/problems/bipartite-graph/1
 VIDEO LINK - https://www.youtube.com/watch?v=ZBhZ1DXGrhA
+             https://www.youtube.com/watch?v=uC884ske2uQ&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=10
  A GRAPH CAN BE BIPARTITE ONLY WHEN - 1. THE GRAPH IS ACYCLIC
                                       2. EVEN IF THE GRAPH IS CYCLIC IT HAS EVEN NUMBER OF VERTICES
 
@@ -69,5 +70,47 @@ class Solution
             this.v = v;
             this.l = l;
         }
+    }
+}
+
+
+-------------------------------------------------------------USING DFS(CONFUSING HAI THODA SEE STRIVER KA VIDEO)-------------------------------------------
+ class Solution
+{
+    public boolean isBipartite(int V, ArrayList<ArrayList<Integer>>adj)
+    {
+        int colour[] = new int[V];
+        Arrays.fill(colour, -1);
+        
+        for(int i=0; i<V;i++){
+            if(colour[i] == -1){
+                if(!dfs(i, adj, colour)){
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+        
+    }
+    
+    public boolean dfs(int i, ArrayList<ArrayList<Integer>>adj, int colour[]){
+        
+        if(colour[i] == -1) colour[i] = 1;
+        
+        for(Integer nbr : adj.get(i)){
+            
+            if(colour[nbr] == -1){
+                colour[nbr] = 1 - colour[i];
+                if(!dfs(nbr, adj, colour)) return false;
+            }
+            
+            else if(colour[nbr] == colour[i]){
+                return false;
+                
+            }
+            
+        }
+        return true;
     }
 }
